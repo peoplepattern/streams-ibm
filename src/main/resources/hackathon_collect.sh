5bed92add74bf60131b78e08019c12a604467275
Steve Blackmon <sblackmon@apache.org>
2015-05-13 17:19:51 -0500

    conf files are in src/main/resources/

--- src/main/resources/hackathon_collect.sh
+++ src/main/resources/hackathon_collect.sh
@@ -7,19 +7,19 @@ docker pull quay.io/peoplepattern/twitter-history-elasticsearch:latest
 docker pull quay.io/peoplepattern/streams-ibm:latest
 
 # Collect statuses from seed user
-docker run --name statuses -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-history-elasticsearch:latest java -Dlogback.configuration=/streams/logback-console-info.xml -Dconfig.file=/streams/pipelines/statuses.conf -cp twitter-history-elasticsearch/target/twitter-history-elasticsearch-0.2-SNAPSHOT.jar org.apache.streams.example.twitter.TwitterHistoryElasticsearch
+docker run --name statuses -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-history-elasticsearch:latest java -Dlogback.configuration=/streams/logback-console-info.xml -Dconfig.file=/streams/pipelines/src/main/resources/statuses.conf -cp twitter-history-elasticsearch/target/twitter-history-elasticsearch-0.2-SNAPSHOT.jar org.apache.streams.example.twitter.TwitterHistoryElasticsearch
 
 # Collect friends of seed user
 docker run --name friends.graph -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-follow-graph:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/src/main/resources/friends.conf -cp twitter-follow-graph-0.2-incubating-SNAPSHOT.jar org.apache.streams.example.graph.TwitterFollowGraph
 
 # Collect statuses from friends
-docker run --name friends.history -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-history-elasticsearch:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/friends.statuses.conf -cp twitter-follow-graph-0.2-SNAPSHOT.jar org.apache.streams.example.twitter.TwitterHistoryElasticsearch
+docker run --name friends.history -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-history-elasticsearch:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/src/main/resources/friends.statuses.conf -cp twitter-follow-graph-0.2-SNAPSHOT.jar org.apache.streams.example.twitter.TwitterHistoryElasticsearch
 
 # Apply Personality Insights Enrichment
 docker run --name users.personality -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/streams-ibm:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/src/main/resources/reindex.conf -cp elasticsearch-reindex-0.2-incubating-SNAPSHOT.jar org.apache.streams.elasticsearch.example.ElasticsearchReindex
 
 # Collect friends of friends
-docker run --name followers.history -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-history-elasticsearch:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/followers.conf -cp twitter-follow-graph-0.2-incubating-SNAPSHOT.jar org.apache.streams.example.graph.TwitterFollowGraph
+docker run --name followers.history -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/twitter-history-elasticsearch:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/src/main/resources/followers.conf -cp twitter-follow-graph-0.2-incubating-SNAPSHOT.jar org.apache.streams.example.graph.TwitterFollowGraph
 
 # Apply Sentiment Enrichment
 docker run --name statuses.sentiment -v `pwd`/streams-ibm:/streams quay.io/peoplepattern/streams-ibm:latest java -Dlogback.configurationFile=/streams/logback.xml -Dconfig.file=/streams/pipelines/src/main/resources/sentiment.conf -cp pipelines-0.3-incubating-SNAPSHOT-jar-with-dependencies.jar com.ibm.hackathon.AlchemyPipeline
